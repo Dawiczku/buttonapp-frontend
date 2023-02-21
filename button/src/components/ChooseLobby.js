7351739ce2d4fb7871a76e2c717695b4b4b3fa8c
Dawiczku <skarupski.dawid@gmail.com>
2023-02-21 13:40:22 +0100

    Fix classNames

--- button/src/components/ChooseLobby.js
+++ button/src/components/ChooseLobby.js
@@ -6,16 +6,18 @@ export default function ChooseLobby() {
       <button className="return">
         <span className="material-symbols-outlined">arrow_back</span>Wstecz
       </button>
-      <h2>Choose Avatar !</h2>
+      <div className="create-lobby-container">
+        <h2>Nickname</h2>
 
-      <img
-        src={require("../avatars/dog-face-color-icon.png")}
-        className="new-avatar"
-        alt="avatar"
-      ></img>
+        <img
+          src={require("../avatars/dog-face-color-icon.png")}
+          className="new-avatar"
+          alt="avatar"
+        ></img>
 
-      <button className="create-lobby-btn">create new lobby</button>
-      <button className="join-lobby-btn">join the lobby</button>
+        <button className="submit-btn create-lobby">create new lobby</button>
+        <button className="submit-btn join-lobby">join the lobby</button>
+      </div>
     </div>
   );
 }
