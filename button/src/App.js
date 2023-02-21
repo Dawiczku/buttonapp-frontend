5302dae8130aa027065bfbc6791badb5c80ab5d8
Dawiczku <skarupski.dawid@gmail.com>
2023-02-21 17:06:05 +0100

    Create a lobby code pop-up

--- button/src/App.js
+++ button/src/App.js
@@ -4,12 +4,13 @@ import Header from "./components/Header";
 import CreateAccount from "./components/CreateAccount";
 import AccountList from "./components/AccountList";
 import Footer from "./components/Foooter";
+import ChooseLobby from "./components/ChooseLobby";
 
 function App() {
   return (
     <>
       <Header />
-      <AccountList />
+      <ChooseLobby />
       <Footer />
     </>
   );
--- button/src/components/ChooseLobby.js
+++ button/src/components/ChooseLobby.js
@@ -1,6 +1,32 @@
 import React from "react";
+import { useRef, useState } from "react";
 
 export default function ChooseLobby() {
+  const createButtonRef = useRef();
+
+  const [code, setCode] = useState("");
+  const [status, setStatus] = useState(false);
+
+  const toggleInputVisibility = () => {
+    setStatus(!status);
+  };
+
+  const handleChange = (event) => {
+    setCode(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    if (code.length < 5) {
+      window.alert("Code too short!");
+      return;
+    }
+
+    setCode(code);
+    setCode("");
+  };
+
   return (
     <div className="account-list">
       <button className="return">
@@ -15,8 +41,37 @@ export default function ChooseLobby() {
           alt="avatar"
         ></img>
 
-        <button className="submit-btn create-lobby">create new lobby</button>
-        <button className="submit-btn join-lobby">join the lobby</button>
+        <button className="submit-btn create-lobby" ref={createButtonRef}>
+          create new lobby
+        </button>
+        <button
+          className={
+            status
+              ? "submit-btn join-lobby disabled"
+              : "submit-btn join-lobby enabled"
+          }
+          onClick={toggleInputVisibility}
+        >
+          join the lobby
+        </button>
+        <div
+          className={
+            status
+              ? "join-lobby-input-container enabled"
+              : "join-lobby-input-container disabled"
+          }
+        >
+          <input
+            className="join-lobby-input"
+            type="text"
+            placeholder="ABC12"
+            onChange={handleChange}
+            value={code}
+            minLength="5"
+            maxLength="5"
+          ></input>
+          <button className="join-lobby-button"></button>
+        </div>
       </div>
     </div>
   );
